a04f431a8981a4f80972d02c50d5d73fb1170689
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-05-08 13:44:58 -0600

    feat - main nav dropdown menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import angleArrow from "../../Assets/Pop Out Arrow";
 import ImprintDigitalLogo from "../../Assets/ImprintDigitalLogo";
 import "./Header.scss";
 import "../../index.css";
 
 export const Header = () => {
+  const [showServices, setShowServices] = useState(false);
   return (
     <section id="header">
-      {ImprintDigitalLogo}
-      <nav className="">
-        <button class="nav-item">
+      <nav>
+        <div className="nav-item logo">{ImprintDigitalLogo}</div>
+        <button
+          class="nav-item services"
+          onMouseEnter={() => setShowServices(true)}
+          onMouseLeave={() => setShowServices(false)}
+        >
           Services <div class="arrow-down" />
         </button>
         <button class="nav-item">
@@ -25,8 +30,35 @@ export const Header = () => {
           Resources <div class="arrow-down" />
         </button>
         <button class="nav-item boxed">Contact {angleArrow}</button>
-        <button class="nav-item drop-down"></button>
+        {/* <button class="nav-item drop-down"></button> */}
       </nav>
+      <div
+        id="dropdown"
+        class={`${showServices ? "visible" : "invisible"}`}
+        onMouseEnter={() => setShowServices(true)}
+        onMouseLeave={() => setShowServices(false)}
+      >
+        <section class="dropdown-section left">
+          <h4 className="title">CMO SERVICES</h4>
+          <ul className="service-list">
+            <li>Vision and Strategy</li>
+            <li>Brand, Story, Execution</li>
+            <li>Brand, Story, Execution</li>
+            <li>Leadership</li>
+          </ul>
+        </section>
+        <div className="vert-line" />
+        <section class="dropdown-section center">{ImprintDigitalLogo}</section>
+        <section class="dropdown-section center">{ImprintDigitalLogo}</section>
+        <div className="vert-line" />
+        <section class="dropdown-section right">
+          <h3>
+            Know more about what a Fractional CMO can do for your business
+          </h3>
+          <input type="text" placeholder="SEND US YOUR EMAIL" />
+          <div className="input-arrow">{angleArrow}</div>
+        </section>
+      </div>
     </section>
   );
 };
