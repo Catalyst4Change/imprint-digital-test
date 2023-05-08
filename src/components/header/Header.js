11e0d16a3c898c19e1ae1ee7eda606ed2a5bd9f0
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-05-08 11:27:18 -0600

    initial commit - header created

---
+++ src/components/header/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import angleArrow from "../../Assets/Pop Out Arrow";
+import ImprintDigitalLogo from "../../Assets/ImprintDigitalLogo";
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <section id="header">
+      {ImprintDigitalLogo}
+      <nav>
+        <button class="nav-item">
+          Services <div class="arrow-down" />
+        </button>
+        <button class="nav-item">
+          Case Studies <div class="arrow-down" />
+        </button>
+        <button class="nav-item">
+          About <div class="arrow-down" />
+        </button>
+        <button class="nav-item">
+          Blog <div class="arrow-down" />
+        </button>
+        <button class="nav-item">
+          Resources <div class="arrow-down" />
+        </button>
+        <button class="nav-item boxed">Contact {angleArrow}</button>
+        <button class="nav-item drop-down"></button>
+      </nav>
+    </section>
+  );
+};
