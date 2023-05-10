2216a92e710148e7c5b69c7f0affa613d42b8958
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-05-10 14:58:38 -0600

    feat - add crosses to hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,11 +3,89 @@ import bullRocket from "../../Assets/BULL ROCKET.gif";
 import "./Hero.scss";
 
 export const Hero = ({ windowWidth }) => {
+  const line = {
+    position: "absolute",
+    width: "20px",
+    height: "2px",
+    backgroundColor: "lightgrey",
+  };
+
   return (
     <main
       id="hero"
-      className={`${windowWidth > 500 ? "horizontal" : "vertical"}`}
+      className={`plus-element ${
+        windowWidth > 700 ? "horizontal" : "vertical"
+      }`}
     >
+      {/* top left cross */}
+      <div
+        style={{
+          ...line,
+          top: 0,
+          left: 0,
+          transform: "translateX(-50%)",
+        }}
+      ></div>
+      <div
+        style={{
+          ...line,
+          top: 0,
+          left: 0,
+          transform: "translateX(-50%) rotate(90deg)",
+        }}
+      ></div>
+      {/* top right cross */}
+      <div
+        style={{
+          ...line,
+          top: 0,
+          right: 0,
+          transform: "translateX(50%)",
+        }}
+      ></div>
+      <div
+        style={{
+          ...line,
+          top: 0,
+          right: 0,
+          transform: "translateX(50%) rotate(90deg)",
+        }}
+      ></div>
+      {/* bottom right cross */}
+      <div
+        style={{
+          ...line,
+          bottom: 0,
+          right: 0,
+          transform: "translateX(50%)",
+        }}
+      ></div>
+      <div
+        style={{
+          ...line,
+          bottom: 0,
+          right: 0,
+          transform: "translateX(50%) rotate(90deg)",
+        }}
+      ></div>
+      {/* top left cross */}
+      <div
+        style={{
+          ...line,
+          bottom: 0,
+          left: 0,
+          transform: "translateX(-50%)",
+        }}
+      ></div>
+      <div
+        style={{
+          ...line,
+          bottom: 0,
+          left: 0,
+          transform: "translateX(-50%) rotate(90deg)",
+        }}
+      ></div>
+
       <div className="hero-copy">
         <h1 className="hero-title">Digital Marketing Solutions That Work</h1>
         <p className="hero-text">
