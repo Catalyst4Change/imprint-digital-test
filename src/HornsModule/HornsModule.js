d0942ac92b2d8a3e58053f81f25b6a283f703528
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-05-15 19:51:31 -0600

    fix - corner card hover rendering

--- src/HornsModule/HornsModule.js
+++ src/HornsModule/HornsModule.js
@@ -46,7 +46,21 @@ export const HornsModule = ({ windowWidth }) => {
       >
         {generateCards()}
       </section>
-      <h4>Hire your new CMO</h4>
+      <div className="horns-footer">
+        <h4 className="footer-text">Hire your new CMO</h4>{" "}
+        <svg
+          width="28"
+          height="16"
+          viewBox="0 0 28 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M27.7071 8.70695C28.0976 8.31643 28.0976 7.68326 27.7071 7.29274L21.3431 0.92878C20.9526 0.538255 20.3195 0.538255 19.9289 0.92878C19.5384 1.3193 19.5384 1.95247 19.9289 2.34299L25.5858 7.99985L19.9289 13.6567C19.5384 14.0472 19.5384 14.6804 19.9289 15.0709C20.3195 15.4614 20.9526 15.4614 21.3431 15.0709L27.7071 8.70695ZM0 8.99985L27 8.99985V6.99985L0 6.99985L0 8.99985Z"
+            fill="#E11F26"
+          />
+        </svg>
+      </div>
     </main>
   );
 };
--- src/components/CornerCard/CornerCardBackground/CornerCardBackground.js
+++ src/components/CornerCard/CornerCardBackground/CornerCardBackground.js
@@ -5,18 +5,22 @@ export const CornerCardBackground = ({ isHovered }) => {
   return (
     <svg viewBox="0 0 347 289" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
+        className={`grey-flag ${isHovered ? "active" : "normal"}`}
         d="M270.446 250.882H250.882L287.973 289H308.352L270.446 250.882Z"
         fill="#C3C3C3"
       />
       <path
+        className={`grey-flag ${isHovered ? "active" : "normal"}`}
         d="M286.911 250.882H275.512L312.559 289H324.772L286.911 250.882Z"
         fill="#C3C3C3"
       />
       <path
+        className={`grey-flag ${isHovered ? "active" : "normal"}`}
         d="M298.911 250.882H291.932L329.287 289H337.087L298.911 250.882Z"
         fill="#C3C3C3"
       />
       <path
+        className={`grey-flag ${isHovered ? "active" : "normal"}`}
         d="M308.346 250.882H304.247L341.551 289H346.47L308.346 250.882Z"
         fill="#C3C3C3"
       />
@@ -25,8 +29,8 @@ export const CornerCardBackground = ({ isHovered }) => {
         fill="#E11F26"
       />
       <path
-        className="main-box"
         d="M53.6775 0.999878L1 55.0654V269.941H346.176V0.999878H53.6775Z"
+        className={`main-box ${isHovered ? "active" : "normal"}`}
       />
     </svg>
   );
