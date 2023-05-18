27256cb61c5db649f123a3e9e0f8f3ffd950777f
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-05-18 15:53:46 -0600

    fix - add color transition on carousel item flags

--- src/components/CarouselContainer/CarouselItem/CarouselItem.js
+++ src/components/CarouselContainer/CarouselItem/CarouselItem.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CarouselItem.scss";
-import greyFlags from "../../../Assets/GreyFlags.svg";
+import SlashFlags from "../../../Assets/SlashFlags.svg";
 import redBar from "../../../Assets/RedBar.svg";
 
 export const CarouselItem = ({ width, margin, title, imgURL }) => {
+  const [hover, setHover] = useState(null);
   return (
     <div
       className="carousel-item"
       style={{ width: `${width}px`, marginRight: `${margin}px` }}
+      onMouseEnter={() => setHover("hovered")}
+      onMouseLeave={() => setHover(null)}
     >
-      <div className="grey-flags"> {greyFlags} </div>
+      <div className={`grey-flags ${hover}`}>{SlashFlags} </div>
       <div className="title-bar"> {redBar}</div>
       <img className="clipped-image" src={imgURL} alt={title} />
       <span className="title-text">{title}</span>
